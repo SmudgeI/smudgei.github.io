a70d7b53339ed80eec4cb296dd030fd4ea7eaec3
SmudgeI <nickachuto@gmail.com>
2024-07-16 19:47:37 -0400

    Update ANTI BYPASS.js

--- ANTI BYPASS.js
+++ ANTI BYPASS.js
@@ -1,6 +1,6 @@
 |------------------------------------------|
 | ANTI BYPASS FOR DESKTOP/LAPTOP COMPUTERS.|
-|                                          |
+|------------------------------------------|
 | FOR PORTABLE COMPUTERS SUCH AS:          |
 | MULTIPURPOSE PHONES AND/OR TABLETS       |
 | ANTI BYPASS IS DEEMED UNNECESSARY.       |
