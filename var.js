e9a5410b28b1c21c4a4b2fd5258b4b343743b1e1
SmudgeI <nickachuto@gmail.com>
2024-07-17 15:30:43 -0400

    Update var.js

--- var.js
+++ var.js
@@ -11,3 +11,47 @@
         var ALERT = 0;
 
         var BRUTEFORCE = 0;
+
+        var l11 = document.getElementById("Answer");
+
+        var l22 = document.getElementById("Answer1");
+
+        var l33 = document.getElementById("Answer2");
+
+        var l44 = document.getElementById("Answer3");
+
+        var l55 = document.getElementById("Answer4");
+
+        var l66 = document.getElementById("Answer5");
+
+        var l77 = document.getElementById("Answer6");
+
+        var l88 = document.getElementById("Answer7");
+
+        var l99 = document.getElementById("Answer8");
+
+        var l21 = document.getElementById("Answer9");
+
+        var letter1 = "ERROR";
+
+        var letter2 = "ERROR";
+
+        var date = 0;
+
+        var C1 = 0;
+
+        var C2 = 0;
+
+        var num = 1;
+
+        var l1 = 100;
+
+        var l2 = 100;
+
+        var positive = l1 + l2;
+
+        var negative = l1 - l2;
+
+        var mode = 0;
+
+        var LOCK = 0;
